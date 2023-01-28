3b27c5b83faa25ea0dc8f0a3fd6b27c1efa1a087
Lenwr <saidabou.lateam@gmail.com>
2023-01-28 19:55:11 +0100

    port update1

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', 'containers-us-west-44.railway.app'),
+      host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 7799),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
